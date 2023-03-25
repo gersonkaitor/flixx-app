26835689102c90459ecfcbdd6423b00c559fd893
gersonkaitor <gersonkaitor@gmail.com>
2023-03-25 18:48:16 +0800

    Spinner & Popular TV Shows

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Display 20 most popular movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('/movie/popular');
 
@@ -37,7 +38,49 @@ async function displayPopularMovies() {
         </div>
       </div>
     `;
+
     document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+// Display 20 most popular TV Shows
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('/tv/popular');
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <div class="card">
+        <a href="movie-details.html?id=${show.id}">
+          ${
+            show.poster_path
+              ? `
+            <img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="#${show.name}e"
+            />
+            `
+              : `
+            <img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="#${show.name}e"
+          />
+            `
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${show.first_air_date}</small>
+          </p>
+        </div>
+      </div>
+    `;
+
+    document.querySelector('#popular-shows').appendChild(div);
   });
 }
 
@@ -46,13 +89,26 @@ async function fetchAPIData(endpoint) {
   const API_KEY = '1013db5a2252bd0991d11f08fe8d248c';
   const API_URL = 'https://api.themoviedb.org/3/';
 
+  showSpinner();
+
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
 
   const data = await response.json();
+  hideSpinner();
 
   return data;
+}
+
+// Show Spinner
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+// Hide Spinner
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
 }
 
 // Highlight active link
@@ -73,7 +129,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Tv Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
