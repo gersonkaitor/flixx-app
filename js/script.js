b330b326b9c0dfce9c57de5a0400bb5224d5b0f6
gersonkaitor <gersonkaitor@gmail.com>
2023-03-26 11:25:03 +0800

    TV Show Details Page

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ async function displayPopularShows() {
     div.classList.add('card');
     div.innerHTML = `
         <div class="card">
-        <a href="movie-details.html?id=${show.id}">
+        <a href="tv-details.html?id=${show.id}">
           ${
             show.poster_path
               ? `
@@ -159,7 +159,84 @@ async function displayMovieDetails() {
   document.querySelector('#movie-details').appendChild(div);
 }
 
-// Display BackDrop on Details PAges
+// Display TV Show Details
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overlay for background image
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+  <div class="details-top">
+    <div>
+    ${
+      show.poster_path
+        ? `
+      <img
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt="#${show.name}e"
+      />
+      `
+        : `
+      <img
+        src="../images/no-image.jpg"
+        class="card-img-top"
+        alt="#${show.name}e"
+    />
+      `
+    }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} /10
+      </p>
+      <p class="text-muted">Release Date: ${show.first_air_date}</p>
+      <p>
+        ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join(' ')}
+      </ul>
+      <a href="${
+        show.homepage
+      }" target="_blank" class="btn">Visit Show Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number Of Episodes:</span> ${
+      show.number_of_episodes
+    }</li>
+    <li><span class="text-secondary">Last Air Date:</span> ${
+      show.last_air_date
+    }</li>
+    <li>
+      <span class="text-secondary">Last Episode To Air:</span> ${
+        show.last_episode_to_air.name
+      }
+    </li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">${show.production_companies
+    .map((company) => `<span>${company.name}</span>`)
+    .join(', ')}</div>
+</div>
+  `;
+
+  document.querySelector('#show-details').appendChild(div);
+}
+
+// Display BackDrop on Details Pages
 async function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement('div');
   overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
@@ -236,7 +313,7 @@ function init() {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('TV Details');
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
